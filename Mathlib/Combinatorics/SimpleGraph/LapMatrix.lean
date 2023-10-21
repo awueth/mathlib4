95a60d3a9ca5ae727ce3f338aefc87ceb1e27086
awueth <adriwu@live.com>
2023-10-21 12:44:31 +0200

    blabla

--- Mathlib/Combinatorics/SimpleGraph/LapMatrix.lean
+++ Mathlib/Combinatorics/SimpleGraph/LapMatrix.lean
@@ -2,56 +2,39 @@ import Mathlib.Combinatorics.SimpleGraph.Basic
 import Mathlib.Combinatorics.SimpleGraph.AdjMatrix
 import Mathlib.Combinatorics.SimpleGraph.IncMatrix
 import Mathlib.Data.Matrix.Basic
+import Mathlib.Data.Real.Basic
+import Mathlib.LinearAlgebra.Matrix.BilinearForm
 
 open BigOperators Finset Matrix SimpleGraph
 
 variable {V : Type*} [Fintype V] [DecidableEq V] (G : SimpleGraph V) [DecidableRel G.Adj]
 
-
-def degMatrix : Matrix V V ℤ :=
+def degMatrix : Matrix V V ℝ :=
   fun a b => if a = b then G.degree a else 0
 
-def lapMatrix : Matrix V V ℤ := degMatrix G - G.adjMatrix ℤ
+def lapMatrix : Matrix V V ℝ := degMatrix G - G.adjMatrix ℝ
+
+def lapBilinForm := Matrix.toBilin' (lapMatrix G)
 
 def cut : Finset V → ℕ :=
   fun s => ∑ i in s, ∑ j in sᶜ, (if G.Adj i j then 1 else 0)
 
 variable (s : Finset V)
 
-#check cut G s
+def special_vector : V → ℝ := fun v => if v ∈ s then 1 else -1
 
-/-
-TODO:
-theorem : cut G s = 1/4 * x^T * lapMatrix G * x
-where x = 1_{S} - 1_{S^c}
--/
-
-
-/-
-Stubid, only works for digraphs
-theorem whatever :
-  lapMatrix G = (G.incMatrix ℤ * (G.incMatrix ℤ)ᵀ) := by
-  apply Matrix.ext
-  intro i j
-  by_cases h : i = j
-
-  -- Case i = j
-  {
-    rw [h]
-    rw [incMatrix_mul_transpose_diag]
-    unfold lapMatrix
-    simp
-    unfold degMatrix
-    simp
-  }
-
-  -- Case i ≠ j
-  {
-    simp [mul_apply, transpose_apply, incMatrix_apply_mul_incMatrix_apply]
-    unfold lapMatrix
-    unfold degMatrix
-    unfold adjMatrix
-    simp [h]
-    sorry
-  }
--/
+/- x^tLx = 4*cut(S) -/
+theorem asdf :
+  lapBilinForm G (special_vector s) (special_vector s) = 4*cut G s := by
+  unfold lapBilinForm
+  rw [Matrix.toBilin'_apply']
+  unfold lapMatrix
+  rw [sub_mulVec]
+  simp
+  unfold mulVec dotProduct
+  simp [Finset.mul_sum]
+  unfold degMatrix
+  simp [mul_comm]
+  unfold special_vector
+  simp
+  sorry
