476986d139478925c60212cfe7738212b45b06cf
awueth <adriwu@live.com>
2023-11-02 11:03:34 +0100

    Made some progress, added comments and made notation more consistent.

--- Mathlib/Combinatorics/SimpleGraph/LapMatrix.lean
+++ Mathlib/Combinatorics/SimpleGraph/LapMatrix.lean
@@ -5,6 +5,7 @@ import Mathlib.Combinatorics.SimpleGraph.AdjMatrix
 import Mathlib.Combinatorics.SimpleGraph.IncMatrix
 import Mathlib.Data.Matrix.Basic
 import Mathlib.Data.Matrix.Rank
+import Mathlib.Data.Fintype.Basic
 import Mathlib.Data.Fintype.Card
 import Mathlib.Data.Real.Basic
 import Mathlib.LinearAlgebra.Basic
@@ -19,6 +20,7 @@ def SimpleGraph.degMatrix (R : Type*) [Ring R] : Matrix V V R :=
 
 def SimpleGraph.lapMatrix (R : Type*) [Ring R] : Matrix V V R := G.degMatrix R - G.adjMatrix R
 
+-- The vector (1,...,1) is in the kernel of the laplacian
 theorem lapMatrix_mulVec_const : mulVec (G.lapMatrix ℤ) (Function.const V 1) = 0 := by
   unfold lapMatrix
   rw [sub_mulVec]
@@ -29,16 +31,17 @@ theorem lapMatrix_mulVec_const : mulVec (G.lapMatrix ℤ) (Function.const V 1) =
   simp only [of_apply, sum_ite_eq, mem_univ, ite_true, sub_self]
   -- Could this be useful: adjMatrix_mulVec_const_apply?
 
+-- The numbers of edges that are "cut" by removing a subset s of vertices
 def cut : Finset V → ℕ :=
   fun s => ∑ i in s, ∑ j in sᶜ, (if G.Adj i j then 1 else 0)
 
 variable (s : Finset V)
 
-def cutIndicator : V → ℤ := fun v => if v ∈ s then 1 else -1
+def cutIndicator : V → ℤ := fun i => if i ∈ s then 1 else -1
 
-lemma cutIndicator_mul_cutIndicator (x y : V) :
-  (cutIndicator s x) * (cutIndicator s y) =
-  if ((x ∈ s ∧ y ∈ s) ∨ (x ∈ sᶜ ∧ y ∈ sᶜ)) then 1 else - 1 := by
+lemma cutIndicator_mul_cutIndicator (i j : V) :
+  (cutIndicator s i) * (cutIndicator s j) =
+  if ((i ∈ s ∧ j ∈ s) ∨ (i ∈ sᶜ ∧ j ∈ sᶜ)) then 1 else - 1 := by
   unfold cutIndicator
   split
   case inl h
@@ -46,45 +49,47 @@ lemma cutIndicator_mul_cutIndicator (x y : V) :
   case inr h'
   · simp [h']
 
-lemma cutIndicator_square (x : V) :
-  (cutIndicator s x) * (cutIndicator s x) = 1 := by
+lemma cutIndicator_square (i : V) :
+  (cutIndicator s i) * (cutIndicator s i) = 1 := by
   unfold cutIndicator
   split
   repeat simp
 
-lemma vadkfboirw :
-  cutIndicator s ⬝ᵥ mulVec (G.degMatrix ℤ) (cutIndicator s) = ∑ x : V, G.degree x := by
+-- xᵀDx = ∑ᵢ dᵢ
+lemma cutIndicator_degMatrix_cutIndicator :
+  cutIndicator s ⬝ᵥ mulVec (G.degMatrix ℤ) (cutIndicator s) = ∑ i : V, G.degree i := by
   unfold mulVec dotProduct
   simp [Finset.mul_sum]
   unfold degMatrix
   simp [mul_comm, ← mul_assoc, cutIndicator_square]
 
-lemma akvoioifke :
-  cutIndicator s ⬝ᵥ mulVec (G.adjMatrix ℤ) (cutIndicator s) = ∑ x : V, (∑ y : V, if G.Adj x y then (cutIndicator s x * cutIndicator s y) else 0) := by
+-- xᵀDx = ∑₍ᵢⱼ₎ xᵢxⱼ
+lemma cutIndicator_adjMatrix_cutIndicator :
+  cutIndicator s ⬝ᵥ mulVec (G.adjMatrix ℤ) (cutIndicator s) =
+  ∑ i : V, (∑ j : V, if G.Adj i j then (cutIndicator s i * cutIndicator s j) else 0) := by -- How to sum over edges (i,j)?
   unfold mulVec dotProduct
   simp only [Finset.mul_sum]
   simp only [mul_comm, ← mul_assoc, cutIndicator_mul_cutIndicator]
   unfold adjMatrix
   simp
 
-/- x^tLx = 4*cut(S) -/
-theorem asdf :
+-- xᵀLx = 4*cut(S)
+theorem cutIndicator_lapMatrix_cutIndicator_equals_four_cut :
   Matrix.toBilin' (G.lapMatrix ℤ) (cutIndicator s) (cutIndicator s) = 4*cut G s := by
   rw [Matrix.toBilin'_apply']
   unfold lapMatrix
   rw [sub_mulVec]
   simp only [dotProduct_sub]
-  rw [vadkfboirw]
-  rw [akvoioifke]
+  rw [cutIndicator_degMatrix_cutIndicator]
+  rw [cutIndicator_adjMatrix_cutIndicator]
   sorry
 
-def cutIndicatorSet (vec : V → ℤ) (_ : ∀x : V, vec x = 1 ∨ vec x = -1) : Set V := {x : V | vec x = 1}
-
-theorem main_result : Fintype.card G.ConnectedComponent = Fintype.card V - (G.lapMatrix ℤ).rank := by
-  sorry
-
-
-
-def badVector : V → ℤ := 2
-
-#check cutIndicatorSet badVector
+-- If there is a vector in the kernel of L other than 1, we can construct a set with cut = 0
+theorem vvkjre2 (y : V → ℤ) (h0 : y ≠ 0) (h_ker : mulVec (G.lapMatrix ℤ) y = 0)
+  (h_ort : y ⬝ᵥ Function.const V 1 = 0) :
+  ∃t : Finset V, Nonempty t ∧ cut G t = 0 := by
+  use {i : V | y i > 0}.toFinset
+  apply And.intro
+  · simp
+    sorry
+  · sorry
